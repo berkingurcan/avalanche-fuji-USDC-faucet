d74264bdf612c881470d39edcbe4d22223bffb90
berkingurcan <70534820+berkingurcan@users.noreply.github.com>
2022-03-19 17:27:06 +0300

    Update App.js
    token adres

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Avalanche Fuji Testnet USDC Faucet</h1>
       <Faucet />
+      <h4>Token address: 0x132b95E8011E01DBf2618d0A2A7FE25114b6b04f</h4>
     </div>
   )
 }
